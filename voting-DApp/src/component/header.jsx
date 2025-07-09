24b8a45ecfb68727df388e7a42a792bbd921a80a
Heng Jun Kai <hengjunkai@gmail.com>
2025-07-09 09:19:18 +0800

    fix bug

--- voting-DApp/src/component/header.jsx
+++ voting-DApp/src/component/header.jsx
@@ -1,53 +1,9 @@
 import { Box, Button } from "@chakra-ui/react";
 import React from "react";
-import { FaVoteYea } from "react-icons/fa";
-import { useContractStore } from "../store/contract.js";
-import { useState, useEffect } from "react";
-import { FiUserPlus } from "react-icons/fi";
-import { FaUserTie } from "react-icons/fa";
-import { TbBuildingCircus } from "react-icons/tb";
-import {
-  Modal,
-  InputLeftElement,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  InputGroup,
-  Input
-} from '@chakra-ui/react';
-import { useDisclosure } from "@chakra-ui/react";
+
 
 function Header() {
-  const {
-    isOpen: isOpenCandidate,
-    onOpen: onOpenCandidate,
-    onClose: onCloseCandidate
-  } = useDisclosure();
 
-  const {
-    isOpen: isOpenVote,
-    onOpen: onOpenVote,
-    onClose: onCloseVote
-  } = useDisclosure();
-
-  const { registerCandidate, castVote } = useContractStore();
-
-  const [candidate, setCandidate] = useState({
-    eventId: "",
-    name: "",
-  });
-
-  const [vote, setVote] = useState({
-    eventId: "",
-    name: "",
-  });
-
-  const handleVote = async () => { };
-
-  const handleCandidate = async () => { };
 
   return (
     <Box
